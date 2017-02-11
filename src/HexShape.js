e86fb169112640c347d203392f22c43fe1045a06
Dave Sag <davesag@gmail.com>
2017-02-11 12:23:54 +0530

    allow adding className property to Hex options which is then used by HexShape, enabling different hexen to be styled differently

--- src/HexShape.js
+++ src/HexShape.js
@@ -1,22 +1,22 @@
 import React from 'react';
-const { object } = React.PropTypes
+import classNames from 'classnames';
 import HexPattern from './HexPattern';
 import HexPointers from './HexPointers';
 import HexUtils from './HexUtils';
 
+const { object } = React.PropTypes;
+
 class HexShape extends React.Component {
 
   getPoints(hex) {
-    let points = this.props.layout.getPolygonPoints(hex)
+    const points = this.props.layout.getPolygonPoints(hex)
 
-    return points.map(point => {
-      return point.x + ',' + point.y;
-    }).join(' ');
+    return points.map(point => `${point.x},${point.y}`).join(' ');
   }
 
   translate() {
-    let hex = this.props.hex;
-    let pixel = HexUtils.hexToPixel(hex, this.props.layout);
+    const { hex, layout } = this.props;
+    const pixel = HexUtils.hexToPixel(hex, layout);
     return `translate(${pixel.x}, ${pixel.y})`;
   }
 
@@ -25,20 +25,24 @@ class HexShape extends React.Component {
   }
 
   render() {
-    let hex = this.props.hex;
-    let text = (hex.props.text) ? hex.props.text : HexUtils.getID(hex);
-    let actions = this.props.actions;
-    let styles = this.getStyles(hex);
-    let points = this.getPoints(hex);
+    const { hex, actions } = this.props;
+    const text = (hex.props.text) ? hex.props.text : HexUtils.getID(hex);
+    const styles = this.getStyles(hex);
+    const points = this.getPoints(hex);
+    const { className } = hex.props;
+
     return (
-      <g className="shape-group" transform={this.translate()} draggable="true"
+      <g
+        className={classNames('shape-group', className)}
+        transform={this.translate()}
+        draggable="true"
         onMouseEnter={e => actions.onMouseEnter(this.props.hex, e)}
         onMouseLeave={e => actions.onMouseLeave(this.props.hex, e)}
         onDragStart={e => actions.onDragStart(this.props.hex, e)}
         onDragEnd={e => actions.onDragEnd(this.props.hex, e)}
         onDragOver={e => actions.onDragOver(this.props.hex, e)}
         onDrop={e => actions.onDrop(this.props.hex, e)}
-        >
+      >
         <HexPattern hex={hex} />
         <polygon points={points} style={styles} />
         <HexPointers hex={hex} points={points} />
