f4c2c8ac5bc68352d74227c40026e433bf727916
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2017-02-18 17:48:43 +0900

    Added event support for V1

--- examples/pathfinding/src/App.js
+++ examples/pathfinding/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import { HexGrid, HexUtils } from 'react-hexgrid';
+import { GridGenerator, HexGrid, Layout, Path, Text, Hexagon, HexUtils } from 'react-hexgrid';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    let boardConfig = {
-      width: 1200, height: 1000,
-      layout: { width: 6, height: 6, flat: true, spacing: 1.0 },
-      origin: { x: 0, y: 0 },
-      map: 'hexagon',
-      mapProps: [ 4 ]
-    }
-    let grid = HexGrid.generate(boardConfig);
+    const hexagons = GridGenerator.hexagon(4);
     this.state = {
-      grid,
-      config: boardConfig,
+      hexagons,
       path: { start: null, end: null }
     };
   }
 
-  onClick(hex, event) {
+  onClick(event, hex) {
     const { path } = this.state;
     if (path.start == null) {
       path.start = hex;
@@ -32,14 +24,14 @@ class App extends Component {
     this.setState({ path });
   }
 
-  onMouseEnter(targetHex, event) {
+  onMouseEnter(event, targetHex) {
     // Set the path's end on hover
-    const { path, grid } = this.state;
+    const { path, hexagons } = this.state;
     path.end = targetHex;
 
     // Color some hexagons
-    const hexagons = grid.hexagons.map(hex => {
-      // Highlight tiles that are next to the target  (1 distance away)
+    const coloredHexas = hexagons.map(hex => {
+      // Highlight tiles that are next to the target (1 distance away)
       hex.props.className = (HexUtils.distance(targetHex, hex) < 2) ? 'active' : '';
 
       // If the tile is on same coordinate, add class specific to the coordinate name
@@ -50,23 +42,36 @@ class App extends Component {
       return hex;
     });
 
-    grid.hexagons = hexagons;
-    this.setState({ path, grid });
+    this.setState({ path, hexagons: coloredHexas });
   }
 
   render() {
-    let { grid, config, path } = this.state;
-    const actions = {
-      onMouseEnter: (h, e) => this.onMouseEnter(h, e),
-      onMouseLeave: () => {},
-      onClick: (h, e) => this.onClick(h, e)
-    };
+    let { hexagons, path } = this.state;
     return (
       <div className="App">
         <h2>Pathfinding & active highlight</h2>
         <p>Click a tile to start drawing a path to your cursor. Click again to cancel.</p>
         <p>Hover around the board to see helper lines drawn.</p>
-        <HexGrid width={config.width} height={config.height} hexagons={grid.hexagons} layout={grid.layout} actions={actions} path={path} />
+        <HexGrid width={1200} height={800}>
+          <Layout size={{ x: 6, y: 6 }} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
+            {
+              hexagons.map((hex, i) => (
+                <Hexagon
+                  key={i}
+                  q={hex.q}
+                  r={hex.r}
+                  s={hex.s}
+                  className={hex.props.className}
+                  onMouseEnter={(e, h) => this.onMouseEnter(e, h)}
+                  onClick={(e, h) => this.onClick(e, h)}
+                >
+                  <Text>{HexUtils.getID(hex)}</Text>
+                </Hexagon>
+              ))
+            }
+            <Path start={path.start} end={path.end} />
+          </Layout>
+        </HexGrid>
       </div>
     );
   }
--- src/Hexagon/Hexagon.js
+++ src/Hexagon/Hexagon.js
@@ -10,22 +10,58 @@ class Hexagon extends Component {
     s: PropTypes.number.isRequired,
     points: PropTypes.string,
     fill: PropTypes.string,
+    className: PropTypes.string,
     layout: PropTypes.object,
+    onMouseEnter: PropTypes.func,
     children: PropTypes.node
   };
 
-  translate() {
-    const { q, r, s, layout } = this.props;
+  constructor(props) {
+    super(props);
+    const { q, r, s, layout } = props;
     const hex = new Hex(q, r, s);
     const pixel = HexUtils.hexToPixel(hex, layout);
-    return `translate(${pixel.x}, ${pixel.y})`;
+
+    this.state = {
+      hex,
+      transform: `translate(${pixel.x}, ${pixel.y})`
+    };
   }
 
+  getActions() {
+    const DEFAULT_ACTIONS = {
+      onMouseEnter: () => {},
+      onMouseLeave: () => {},
+      onDragStart: () => {},
+      onDragEnd: () => {},
+      onDragOver: () => {},
+      onDrop: () => {},
+      onClick: () => {}
+    };
+    // TODO This object will have other props now as well
+    return Object.assign({}, DEFAULT_ACTIONS, this.props);
+  }
+
+  // TODO ComponentWillReceiveProps to update state
   render() {
-    const { points, fill } = this.props;
+    const { points, fill, className } = this.props;
+    const { hex, transform } = this.state;
+    const actions = this.getActions();
     const fillId = (fill) ? `url(#${fill})` : null;
+    // TODO Would be better not to bind the events at all, if the event prop is not defined
     return (
-      <g className="shape-group" transform={this.translate()}>
+      <g
+        className={classNames('shape-group', className)}
+        transform={transform}
+        onMouseEnter={e => actions.onMouseEnter(e, hex)}
+        onMouseEnter={e => actions.onMouseEnter(e, hex)}
+        onMouseLeave={e => actions.onMouseLeave(e, hex)}
+        onDragStart={e => actions.onDragStart(e, hex)}
+        onDragEnd={e => actions.onDragEnd(e, hex)}
+        onDragOver={e => actions.onDragOver(e, hex)}
+        onDrop={e => actions.onDrop(e, hex)}
+        onClick={e => actions.onClick(e, hex)}
+      >
         <polygon points={points} fill={fillId} />
         {this.props.children}
       </g>
