0a6de2b5e7fa158200d640f845151d30223008ae
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2017-04-20 19:56:51 +0900

    v1.0 RC-1 WIP animations example

--- examples/animations/src/App.js
+++ examples/animations/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HexGrid, Layout, Hexagon, GridGenerator } from 'react-hexgrid';
 import './App.css';
 import './Animations.css';
+import './Filters.css';
 
 class App extends Component {
   // Some sick animations to hexagons
@@ -10,10 +11,27 @@ class App extends Component {
     return (
       <div className="App">
         <h1>SVG animations with react-hexgrid</h1>
-        <HexGrid width={1200} height={800}>
-          <Layout size={{ x: 7, y: 7 }} origin={{ x: -15, y: -40 }}>
+        <small>Work in progress...</small>
+        <HexGrid width={1200} height={900}>
+          <Layout size={{ x: 6, y: 6 }} origin={{ x: -15, y: -40 }} spacing={1.15}>
             { hexagons.map((hex, i) => <Hexagon key={i} {...hex} />) }
           </Layout>
+          <defs>
+            <filter id="shadowed-goo">
+              <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+              <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+              <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+              <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+              <feComposite in2="shadow" in="goo" result="goo" />
+              <feComposite in2="goo" in="SourceGraphic" result="mix" />
+            </filter>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+              <feComposite in2="goo" in="SourceGraphic" result="mix" />
+            </filter>
+          </defs>
         </HexGrid>
       </div>
     );
