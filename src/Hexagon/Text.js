72eee9d90451e9e898312a5915b33e8ed0769cae
Luis Fernando Rocha <luisfrocha@icloud.com>
2018-01-12 14:13:47 -0600

    Allow Text to be positioned and styled
    Allow Text to be positioned and styled independently

--- src/Hexagon/Text.js
+++ src/Hexagon/Text.js
@@ -4,13 +4,16 @@ import PropTypes from 'prop-types';
 // TODO Text is a separate component so that it could wrap the given text inside the surrounding hexagon
 class Text extends Component {
   static propTypes = {
-    children: PropTypes.string
+    children: PropTypes.string,
+    x: PropTypes.number,
+    y: PropTypes.number,
+    className: PropTypes.string
   };
 
   render() {
-    const { children } = this.props;
+    const { children, x, y, className } = this.props;
     return (
-      <text x="0" y="0.3em" textAnchor="middle">{children}</text>
+      <text x={x || 0} y={y ? y : '0.3em'} className={className} textAnchor="middle">{children}</text>
     );
   }
 }
