5b2afb7be96ded3030190f03b1bcbf29b9c9fca6
Jannik <sisosys6@gmail.com>
2022-07-02 11:15:30 +0200

    internal (Storybook): add presets to enable emotion

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  babel: async (options) => ({
+    ...options,
+    presets: [
+      ["@babel/preset-env", { shippedProposals: true, loose: true }],
+      [
+        "@babel/preset-react",
+        { runtime: "automatic", importSource: "@emotion/react" },
+      ],
+      ["@babel/preset-typescript"],
+    ],
+  }),
 }
