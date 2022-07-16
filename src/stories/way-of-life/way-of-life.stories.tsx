bfb4eb1c577fc017a304ba78c0e8a09245432f59
Jannik <sisosys6@gmail.com>
2022-07-16 14:54:03 +0200

    improve 'way of life' storybook example

--- src/stories/way-of-life/way-of-life.stories.tsx
+++ src/stories/way-of-life/way-of-life.stories.tsx
@@ -6,14 +6,9 @@ import { useInterval } from "react-use"
 import { COLORS } from "../colors"
 
 export default {
-  title: "Wayoflife",
+  title: "Way of life",
   component: Hexagon,
 } as ComponentMeta<typeof Hexagon>
-
-const elementSize = 5
-const count = 5
-
-const initialHexagons = GridGenerator.hexagon(count)
 
 type State = "Dead" | "Living"
 
@@ -21,25 +16,25 @@ type Coordinates = { q: number; r: number; s: number }
 
 type Cell = Coordinates & { state: State }
 
-const hexas: Cell[] = initialHexagons.map(({ q, r, s }) => ({
-  ...{ q, r, s },
-  state: (Math.random() < 0.15 ? "Living" : "Dead") as State,
-}))
-
 type CellDict = { [coords: string]: Cell }
 
-const dict: CellDict = {}
-
-hexas.forEach((hex) => {
-  const { q, r, s } = hex
-  dict[`${q}-${r}-${s}`] = hex
-})
-const rules = `Each cell with one or no neighbors dies from isolation.
-Each cell with three or more neighbors dies from overpopulation.
-Only the cells with two neighbors survives.
-
-Dead cells
-Each cell with two neighbors revives.`
+function reset() {
+  const count = 8
+  const initialHexagons = GridGenerator.hexagon(count)
+  const dict: CellDict = {}
+  const hexas: Cell[] = initialHexagons.map(({ q, r, s }) => ({
+    ...{ q, r, s },
+    state: (Math.random() < 0.4 &&
+    HexUtils.distance({ q, r, s }, { q: 0, r: 0, s: 0 }) <= 3
+      ? "Living"
+      : "Dead") as State,
+  }))
+  hexas.forEach((hex) => {
+    const { q, r, s } = hex
+    dict[`${q}-${r}-${s}`] = hex
+  })
+  return dict
+}
 
 function getNeighbors(hex: Cell, dict: CellDict) {
   let neighbors = HexUtils.neighbors(hex)
@@ -53,13 +48,19 @@ function countLivingNeighbors(hex: Cell, dict: CellDict) {
   return neighbors.reduce((v1, v2) => v1 + (v2.state === "Living" ? 1 : 0), 0)
 }
 
+const colors = [COLORS.teal]
+const elementSize = 2.7
 const size = { x: elementSize, y: elementSize }
 
-const colors = [COLORS.blue, COLORS.cyan, COLORS.teal]
-
 const Template: ComponentStory<typeof Hexagon> = (args, { argTypes }) => {
-  const [hexagons, setHexagons] = React.useState<CellDict>(dict)
+  const [hexagons, setHexagons] = React.useState<CellDict>({})
+  React.useEffect(() => {
+    const hexas = reset()
+    setHexagons(hexas)
+  }, [])
   const [step, setI] = React.useState(0)
+  const [speed, setSpeed] = React.useState(20)
+  const timing = speed / 20.0
 
   useInterval(() => {
     const result: CellDict = {}
@@ -77,7 +78,14 @@ const Template: ComponentStory<typeof Hexagon> = (args, { argTypes }) => {
 
     setHexagons(result)
     setI(step + 1)
-  }, 3000)
+  }, timing * 1000)
+
+  const dyingAnimationDuration = timing * 1.5
+  const revivingAnimationDuration = timing * 0.8
+
+  const timingFunctionDying = `fill ${dyingAnimationDuration}s cubic-bezier(0.7, 0.8, 0.9, 1)`
+  const timingFunctionReviving = `fill ${revivingAnimationDuration}s cubic-bezier(0.2, 0.5, 0.9, 1)`
+
   return (
     <div
       style={{ background: COLORS.gray[1] }}
@@ -88,53 +96,31 @@ const Template: ComponentStory<typeof Hexagon> = (args, { argTypes }) => {
         // background: #f0f0f0;
       `}
     >
-      {/* <button
-        onClick={() => {
-          const result: CellDict = {}
-          Object.keys(hexagons).forEach((coords) => {
-            const element = hexagons[coords]
-            const { q, r, s, state } = element
-            let livingNeighbors = countLivingNeighbors(element, hexagons)
-
-            if (livingNeighbors == 2) {
-              result[`${q}-${r}-${s}`] = { ...element, state: "Living" }
-            } else {
-              result[`${q}-${r}-${s}`] = { ...element, state: "Dead" }
-            }
-          })
-
-          setHexagons(result)
-        }}
-      >
-        Tick
-      </button> */}
-      <div>{rules}</div>
-      <HexGrid
-        width={800}
-        height={600}
-        css={css`
-          g {
-            fill: #3f51b5;
-            fill-opacity: 0.9;
-            &:hover {
-              fill-opacity: 1;
-            }
-            text {
-              font-size: 0.2em;
-              fill: #000;
-              fill-opacity: 0.9;
-              transition: fill-opacity 1.9s;
-              transition: fill 1.9s;
-            }
-            polygon {
-              stroke: gray;
-              stroke-width: 0.1;
-              transition: fill-opacity 1.9s;
-              transition: fill 2s ease;
-            }
-          }
-        `}
-      >
+      <div>
+        <p>
+          <b>Living cells: </b>
+          Each cell with one or no neighbors dies from isolation. Each cell with
+          three or more neighbors dies from overpopulation. Only the cells with
+          two neighbors survive.
+        </p>
+        <p>
+          <b>Dead cells:</b> Each cell with two neighbors revives.
+        </p>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            const newGrid = reset()
+            setHexagons(newGrid)
+          }}
+        >
+          Reset
+        </button>{" "}
+        <button onClick={() => setSpeed(speed + 1)}>-</button>{" "}
+        <button onClick={() => setSpeed(Math.max(speed - 1, 1))}>+</button>{" "}
+        speed: {speed}
+      </div>
+      <HexGrid width={800} height={800} viewBox="-40 -40 100 100">
         <Layout size={size} flat={true} spacing={1}>
           {Object.keys(hexagons)
             .map((v) => hexagons[v])
@@ -144,23 +130,28 @@ const Template: ComponentStory<typeof Hexagon> = (args, { argTypes }) => {
                 q={hex.q}
                 r={hex.r}
                 s={hex.s}
-                cellStyle={{
-                  fill:
-                    hex.state === "Dead"
-                      ? COLORS.gray[4]
-                      : colors[step % colors.length][7],
-                }}
-              >
-                {/* <Text>{hex.state}</Text> */}
-                {/* <Text style={{ fontSize: "0.1rem" }}>
-                  {HexUtils.getID(hex)}
-                </Text> */}
-                {/* <Text
-                  style={{ fill: "red" }}
-                >
-                  {countLivingNeighbors(hex, hexagons)}
-                </Text> */}
-              </Hexagon>
+                css={css`
+                  g {
+                    polygon {
+                      fill: ${hex.state === "Dead"
+                        ? COLORS.gray[
+                            HexUtils.distance(hex, { q: 0, r: 0, s: 0 }) % 4
+                          ]
+                        : colors[step % colors.length][
+                            5 +
+                              (HexUtils.distance(hex, { q: 0, r: 0, s: 0 }) % 3)
+                          ]};
+                      stroke: ${COLORS.dark[4]};
+                      stroke-opacity: ${0.6 /
+                      HexUtils.distance(hex, { q: 0, r: 0, s: 0 })};
+                      stroke-width: 0.15;
+                      transition: ${hex.state === "Dead"
+                        ? timingFunctionDying
+                        : timingFunctionReviving};
+                    }
+                  }
+                `}
+              />
             ))}
         </Layout>
       </HexGrid>
